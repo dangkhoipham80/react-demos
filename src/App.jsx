64ff544b4e121696743e23307b9f233cd5759d5e
Phạm Đăng Khôi <dangkhoipham80@gmail.com>
2025-03-18 13:06:55 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Add from "./components/Add";
+import Greet from "./components/Greet";
+import Greeting from "./components/Greeting";
+
+function App() {
+  return (
+    <div>
+      <Greet />
+      <Greeting />
+      <Add />
+    </div>
+  )
+};
+
+export default App;
---
+++ src/components/Greeting.jsx
@@ -0,0 +1,5 @@
+const Greeting = () => {
+    return <h1>Hello Greeting</h1>
+};
+
+export default Greeting;
