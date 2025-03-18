8272977d90aa5024794ff1c4ceaf6fa5d2bff979
Phạm Đăng Khôi <dangkhoipham80@gmail.com>
2025-03-18 15:25:02 +0700

    event basic, onClick()

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
-import StyleCard from "./components/StyleCard";
-import ProfileCard from "./components/ProfileCard";
-import IconComponent from "./components/iconComponent";
+const Button = () => {
+  const handleClick = () => console.log(Math.round(Math.random() * 10))
+  return <button onClick={handleClick}>Click</button>
+}
+
+const Copy = () => {
+  const copyHandler = () => {
+    console.log("Stop stealing my content")
+  }
+  return (
+    <p onCopy={copyHandler}>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis cum minus nobis ullam aperiam? Quidem, obcaecati numquam. Harum, perferendis.
+    </p>
+  )
+}
+
+const Move = () => {
+  const moveHandler = () => {
+    alert("Mouse move event fired")
+    console.log("Mouse move event fired")
+  }
+  return (
+    <p onMouseMove={moveHandler}>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam veritatis doloremque laudantium ullam repudiandae, vitae velit modi est exercitationem sit!
+    </p>
+  )
+}
 
 function App() {
   return (
-    <div>
-      <StyleCard />
-      <ProfileCard />
-      <IconComponent />
-    </div>
+    <section>
+      <Button />
+      <Copy />
+      <Move />
+    </section>
   )
 };
 
